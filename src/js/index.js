5f2e41c51c628025f7b99bba76269d55a873605f
Minh Thiện <nmthien2984@gmail.com>
2016-06-07 21:46:03 +0700

    load phones

---
+++ src/js/index.js
@@ -0,0 +1,46 @@
+app = angular.module('app', ["firebase"]);
+
+app.controller("appCtrl", ["$scope", "$firebaseObject", function($scope, $firebaseObject){
+
+	var ref = new Firebase("https://mobileds.firebaseio.com/");
+	
+	$scope.phones = {};
+	$scope.data = $firebaseObject(ref);
+	$scope.data.$loaded()
+  		.then(function() {
+    		console.log($scope.data);
+  		})
+  		.catch(function(err) {
+    		console.error(err);
+  		});
+
+
+  	$scope.loadPhones = function(brand) {
+
+  		$scope.phones = {};
+  		/*console.log($scope.loaded);
+  		if ($scope.loaded == false) {
+  			ref.child("products").once("value", function(snapshot) {				
+					snapshot.forEach(function(childSnapshot) {
+					var key = childSnapshot.key();
+					var childData = childSnapshot.val();
+					if (childData.maker.name == "Apple")
+						$scope.phones.push(childSnapshot);
+				});
+			});
+
+  		} else*/ 
+  			console.log($scope.data.products);	
+
+	  		for (i = 0; i < $scope.data.products.$length; i++) {
+	  			console.log($scope.data.products[i].maker.name);
+				if ($scope.data.products[i].maker.name == brand)
+					$scope.phones.push($scope.data.products[i]);
+			}
+			
+			console.log($scope.phones);
+  		
+  		
+  	}	
+
+}]);
